778216ca53e307b9fa5391f11b030328d86d2669
Wevton Junior <wevtonj@gmail.com>
2024-09-25 12:14:44 -0300

    Agora sim o codigo tá certo

--- Topic Problems/FFT/Apples and Bananas.cpp
+++ Topic Problems/FFT/Apples and Bananas.cpp
@@ -1,5 +1,8 @@
+// https://cses.fi/problemset/task/2111/
+
 /*
-  https://cses.fi/problemset/task/2121/
+    Uso clássico de FFT pra resolver problemas. Transforme os dois vetores em polinômios e interprete os seus valores do vetor como expoentes do polinômio.
+    Depois disso, aplicar o procedimento da FFT pra somar os expoentes e imprimir a resposta.
 */
 
 #include <bits/stdc++.h>
@@ -8,150 +11,109 @@ using namespace std;
 typedef long long ll;
  
 #define all(x) x.begin(), x.end()
+#define debug cout
 #define ft first
 #define sd second
 #define pb push_back
-#define mp make_pair
-#define eb emplace_back
-
-template <class T = int>
-class MCMF {
-public:
-    struct Edge {
-        Edge(int a, T b, T c) : to(a), cap(b), cost(c) {}
-        int to;
-        T cap, cost;
-    };
-
-    MCMF(int size) {
-        n = size;
-        edges.resize(n);
-        pot.assign(n, 0);
-        dist.resize(n);
-        visit.assign(n, false);
+ 
+const int N = 2e5+3;
+ 
+using cd = complex<double>;
+const double PI = acos(-1);
+ 
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+ 
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+ 
+        if (i < j)
+            swap(a[i], a[j]);
     }
-
-    std::pair<T, T> mcmf(int src, int sink, T K) {
-        std::pair<T, T> ans(0, 0);
-        fixPot();
-        while(ans.ft < K){
-            if(!SPFA(src, sink)) break;
-            auto flow = augment(src, sink, K-ans.first);
-            ans.first += flow.first;
-            ans.second += flow.first * flow.second;
-            fixPot();
-        }
-        if(ans.ft < K)
-            return {-1,-1};
-        return ans;
-    }
-
-    void addEdge(int from, int to, T cap, T cost) {
-        edges[from].push_back(list.size());
-        list.push_back(Edge(to, cap, cost));
-        edges[to].push_back(list.size());
-        list.push_back(Edge(from, 0, -cost));
-    }
-private:
-    int n;
-    std::vector<std::vector<int>> edges;
-    std::vector<Edge> list;
-    std::vector<int> from;
-    std::vector<T> dist, pot;
-    std::vector<bool> visit;
-
-    std::pair<T, T> augment(int src, int sink, T flow_) {
-        std::pair<T, T> flow = {flow_, 0};
-        for(int v = sink; v != src; v = list[from[v]^1].to) {
-            flow.first = std::min(flow.first, list[from[v]].cap);
-            flow.second += list[from[v]].cost;
-        }
-        for(int v = sink; v != src; v = list[from[v]^1].to) {
-            list[from[v]].cap -= flow.first;
-            list[from[v]^1].cap += flow.first;
-        }
-        return flow;
-    }
-
-    /* bool dij(int src, int sink) {
-        T INF = std::numeric_limits<T>::max();
-        dist.assign(n, INF);
-        from.assign(n, -1);
-        visit.assign(n, false);
-        dist[src] = 0;
-        for(int i = 0; i < n; i++) {
-            int best = -1;
-            for(int j = 0; j < n; j++) {
-                if(visit[j]) continue;
-                if(best == -1 || dist[best] > dist[j]) best = j;
-            }
-            if(dist[best] >= INF) break;
-            visit[best] = true;
-            for(auto e : edges[best]) {
-                auto ed = list[e];
-                if(ed.cap == 0) continue;
-                T toDist = dist[best] + ed.cost + pot[best] - pot[ed.to];
-                assert(toDist >= dist[best]);
-                if(toDist < dist[ed.to]) {
-                    dist[ed.to] = toDist;
-                    from[ed.to] = e;
-                }
+ 
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
             }
         }
-        return dist[sink] < INF;
-    }*/
-
-    std::queue<int> q;
-    bool SPFA(int src, int sink) {
-        T INF = numeric_limits<T>::max();
-        dist.assign(n, INF);
-        from.assign(n, -1);
-        q.push(src);
-        dist[src] = 0;
-        while(!q.empty()) {
-            int on = q.front();
-            q.pop();
-            visit[on] = false;
-            for(auto e : edges[on]) {
-                auto ed = list[e];
-                if(ed.cap == 0) continue;
-                T toDist = dist[on] + ed.cost + pot[on] - pot[ed.to];
-                if(toDist < dist[ed.to]) {
-                    dist[ed.to] = toDist;
-                    from[ed.to] = e;
-                    if(!visit[ed.to]) {
-                        visit[ed.to] = true;
-                        q.push(ed.to);
-                    }
-                }
-            }
-        }
-        return dist[sink] < INF;
     }
-
-    void fixPot() {
-        T INF = numeric_limits<T>::max();
-        for(int i = 0; i < n; i++) {
-            if(dist[i] < INF) pot[i] += dist[i];
-        }
+ 
+    if (invert) {
+        for (cd & x : a)
+            x /= n;
     }
-};
-
+}
+ 
+void mult(vector<ll> const& a, vector<ll> const& b, vector<ll> & res) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+ 
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+ 
+    res.resize(n);
+    for (int i = 0; i < n; i++)
+        res[i] = round(fa[i].real());
+ 
+    // multiplying polynomials.
+ 
+    // int carry = 0;
+    // for (int i = 0; i < n; i++){
+    //     result[i] += carry;
+    //     carry = result[i] / 10;
+    //     result[i] %= 10;
+    // }
+}
+ 
+void test(){
+    int k, n, m;
+    cin >> k >> n >> m;
+ 
+    vector<ll> m1(k+1, 0), m2(k+1, 0);
+    for(int i = 0, x; i < n; i++){
+        cin >> x;
+        m1[x]++;
+    }
+    for(int i = 0, x; i < m; i++){
+        cin >> x;
+        m2[x]++;
+    }
+ 
+    vector<ll> ans;
+    mult(m1, m2, ans);
+    for(int i = 2; i <= 2*k; i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<"\n";
+}
+ 
 int32_t main(){
-    cin.tie(nullptr)->sync_with_stdio(false);
-
-    int n, m, k;
-    cin >> n >> m >> k;
-
-    MCMF<ll> f(n+1);
-
-    for(int i=0; i<m; i++){
-        int a, b, r, c;
-        cin >> a >> b >> r >> c;
-        f.addEdge(a,b,r,c);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int teste = 1;
+    // cin >> teste;
+ 
+    while(teste--){
+        test();
     }
-    auto rs = f.mcmf(1,n,k);
-    cout<<rs.sd<<"\n";
-
+ 
     return 0;
 }
