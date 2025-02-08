0cfc8c2ca8a79942ba752693ecf6d2243cc4f3ef
Wevton Junior <wevtonj@gmail.com>
2025-02-08 17:12:26 -0300

    Coloquei alguns comentários sobre o algoritmo.

--- Lib/Data Structures/MO.cpp
+++ Lib/Data Structures/MO.cpp
@@ -4,15 +4,12 @@
 */
 
 struct query{
-    static const int K = 350;
-    int l, r, idx;
+    static const int K = 350; // constante milagrosa do sqrt(N).
+    int l, r, idx; // left, right e indice da query.
 
-    query(int _l, int _r, int _idx) { // thing so you can emplace_back
-        this->l = _l;
-        this->r = _r;
-        this->idx = _idx;
-    }
+    query(int _l, int _r, int _idx) : l(_l), r(_r), idx(_idx) {}
 
+    // operador de desigualdade para ordenar as queries.
     bool operator <(const query &o){
         if(l/K == o.l/K){
             if((l/K)&1){
@@ -28,52 +25,60 @@ struct MO{
     /*
         In here we can change everything. Add more things if you think it is necessary for solving your problem, be it more variables, vectors and such.
     */
-    vector<query> queries;
-    vector<int> rs;
+    vector<query> & queries; // vetor para guardar as queries.
+    vector<int> rs; // vetor que guarda as respostas.
 
-    MO(vector<query> queries){
-        this->queries = queries;
-    }
+    MO(vector<query> & queries) : queries(queries){}
 
+    // adiciona esse cara na resposta.
     void add(int x){
       
     }
 
+    // remove esse cara da resposta.
     void rmv(int x){
       
     }
 
+    // calcula a resposta dessa query.
     int calc(){
       
     }
 
     void run(){
-        sort(all(queries));
-        rs.resize(queries.size());
+        sort(queries.begin(), queries.end()); // ordena tudo.
+        rs.resize(queries.size()); // coloca o tamanho correto nas respostas.
 
-        int i = 0, j = -1; // j is always less than i by one in the beginning.
-        for(auto [l, r, idx]: queries){ // ALWAYS do j before i, unless u really like getting runtime errors.
-            while(j < r){
+        /*
+            Pelo amor de Deus ande com a variável j antes de andar a variável i, pelo simples fato de que, no começo, 
+            ela vai estar removendo coisas da sua resposta. Então imagine o estrago que isso causa quando você tenta
+            remover itens inexistentes de um set por exemplo. Isso pode ser ruim. Então ande com a variável j sempre em
+            primeiro lugar, se você for montar seu próprio MO baseado nesse, em qualquer MO que você for fazer na sua vida
+            inteira.
+        */
+        int i = 0, j = -1;
+        for(auto [l, r, idx]: queries){
+            while(j < r){ // coloca coisas na minha resposta e anda pra frente.
                 j++;
-                add(a[j]);
+                add(j);
             }
 
-            while(j > r){
-                rmv(a[j]);
+            while(j > r){ // remove coisas da minha resposta e anda pra trás.
+                rmv(j);
                 j--;
             }
 
-            while(i > l){
+            while(i > l){ // coloca coisas na minha resposta e anda pra trás.
                 i--;
-                add(a[i]);
+                add(i);
             }
 
-            while(i < l){
-                rmv(a[i]);
+            while(i < l){ // remove coisas da minha resposta e anda pra frente.
+                rmv(i);
                 i++;
             }
 
-            rs[idx]=calc();
+            rs[idx]=calc(); // calcula a resposta atual.
         }
     }
 };
