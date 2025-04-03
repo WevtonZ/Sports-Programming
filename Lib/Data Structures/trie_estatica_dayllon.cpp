58b706cf3e7e862f09d4860523d9337aea5932a8
Wevton Junior <wevtonj@gmail.com>
2025-04-03 17:39:01 -0300

    Create trie_estatica_dayllon.cpp

---
+++ Lib/Data Structures/trie_estatica_dayllon.cpp
@@ -0,0 +1,38 @@
+const int N = 1e6+10, K = 26;
+
+struct Node {
+    int child[K];
+    bool leaf;
+
+    Node() {
+        memset(child,0,sizeof(child));
+        leaf=false;
+    }
+};
+
+Node trie[N*K];
+int ct[N*K];
+int tam = 0;
+
+void insert(const string & s) {
+    int at = 0;
+    ct[at]++;
+    for(auto ch: s) {
+        int pos = ch-'a';
+        if(trie[at].child[pos] == 0) trie[at].child[pos] = ++tam;
+        at = trie[at].child[pos];
+        ct[at]++;
+    }
+
+    trie[at].leaf=true;
+}
+
+void erase(const string & s) {
+    int at=0;
+    for(auto ch: s) {
+        int pos = ch-'a';
+        at = trie[at].child[pos];
+        ct[at]--;
+    }
+    if(ct[at]==0) trie[at].leaf=false;
+}
