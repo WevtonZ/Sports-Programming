6d008842dea67d8107a3878c7674ef5de19acb54
Wevton Junior <wevtonj@gmail.com>
2025-04-04 15:11:22 -0300

    Tirar o erase e mudar o nome do algoritmo.

--- Lib/Data Structures/trie_estatica.cpp
+++ Lib/Data Structures/trie_estatica.cpp
@@ -1,38 +1,26 @@
+// N é o tamanho da maior string na trie. K é a profundidade da árvore.
+// como isso nao funciona muito bem pra mim, eu só boto um tamanho aleatório.
 const int N = 1e6+2, K = 18;
 
 struct Node {
     int child[26];
     bool leaf;
 
-    Node() {
+    Node() : leaf(false) {
         memset(child,0,sizeof(child));
-        leaf=false;
     }
 };
 
 Node trie[N*K];
-int ct[N*K];
 int tam = 0;
 
 void insert(const string & s) {
     int at = 0;
-    ct[at]++;
     for(auto ch: s) {
         int pos = ch-'a';
         if(trie[at].child[pos] == 0) trie[at].child[pos] = ++tam;
         at = trie[at].child[pos];
-        ct[at]++;
     }
 
     trie[at].leaf=true;
-}
-
-void erase(const string & s) {
-    int at=0;
-    for(auto ch: s) {
-        int pos = ch-'a';
-        at = trie[at].child[pos];
-        ct[at]--;
-    }
-    if(ct[at]==0) trie[at].leaf=false;
 }
